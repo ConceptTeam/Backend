b1a9dbec6f2a2c7b5448d83a563d1df6e292e096
alexandre-bismuth <alexandre.bismuth@polytechnique.edu>
2023-12-09 17:06:29 +0100

    subClasses Development

--- src/database/src/note_folder_classes.cpp
+++ src/database/src/note_folder_classes.cpp
@@ -10,15 +10,13 @@
 
 using namespace sqlite_orm;
 
-// NEXT STEP: COMBINE THE SAVE AND UPDATE FUNCTIONS INTO ONE FUNCTION IN BASEMODEL ~Alex
-// I'm done for today, plenty must be incorrect, but I did as much as I could.
+// NEXT STEP: COMBINE THE SAVE AND UPDATE FUNCTIONS INTO ONE FUNCTION IN BASEMODEL ~Alex and Peter
 
 class BaseModel
 {
 public:
     BaseModel(std::string table_name, std::vector<std::pair<std::string, std::string>> cols)
         : table_name(table_name), cols(cols) {}
-
     // Might be able to get rid of these 2 as the members are protected and not private
     std::vector<std::pair<std::string, std::string>> get_cols() { return cols; }
     std::string get_table_name() { return table_name; }
@@ -83,10 +81,9 @@ class Folder : BaseModel
         auto storage = make_storage("concept.db",
                                     make_table(table_name,
                                                make_column("title", &Folder::title),
-                                               make_table("notes",
+                                               make_table("folder",
                                                           make_column("title", &Note::title),
-                                                          make_column("content", &Note::content),
-                                                          make_column("last_modified", &Note::last_modified))));
+                                                          make_column("notes", &Folder::notes))));
         storage.sync_schema();
 
         // Insert the new folder into the database
@@ -99,17 +96,49 @@ class Folder : BaseModel
         }
     }
 
-    void filter(bool sortByDate)
+    void update(std::string title, std::vector<Note> notes)
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("title", &Folder::title),
+                                               make_table("folder",
+                                                          make_column("title", &Note::title),
+                                                          make_column("notes", &Folder::notes))));
+        storage.sync_schema();
+
+        // Insert the new folder into the database
+        storage.update(*this);
+
+        // Insert all the notes into the database
+        for (auto &note : notes)
+        {
+            storage.update(note);
+        }
+    }
+
+    void add_note(Note new_note, std::vector<Note> &notes)
+    {
+        for (auto &note : notes)
+        {
+            if (new_note.title == note.title)
+            {
+                throw std::invalid_argument("This title is already chosen in that folder");
+            }
+        }
+        notes.push_back(new_note);
+    }
+
+    void sort_notes(bool sortByDate, bool increasing)
     {
         if (sortByDate)
         {
-            std::sort(notes.begin(), notes.end(), [](Note &a, Note &b)
-                      { return a.get_last_modified() < b.get_last_modified(); });
+            std::sort(notes.begin(), notes.end(), [increasing](Note &a, Note &b)
+                      { return increasing ? a.get_last_modified() < b.get_last_modified() : a.get_last_modified() > b.get_last_modified(); });
         }
         else
         {
-            std::sort(notes.begin(), notes.end(), [](Note &a, Note &b)
-                      { return a.get_title() < b.get_title(); });
+            std::sort(notes.begin(), notes.end(), [increasing](Note &a, Note &b)
+                      { return increasing ? a.get_title() < b.get_title() : a.get_title() > b.get_title(); });
         }
     }
 
@@ -143,12 +172,12 @@ public:
         }
     }
 
-    int Fetch()
+    int fetch()
     {
         return time_spent;
     }
 
-    void Update(int delta)
+    void update(int delta)
     {
         time_spent += delta;
         auto storage = make_storage("concept.db",
@@ -158,7 +187,7 @@ public:
         storage.replace(*this); // Update the time in the database
     }
 
-    void Reset()
+    void reset()
     {
         time_spent = 0;
         auto storage = make_storage("concept.db",
@@ -170,4 +199,63 @@ public:
 
 private:
     int time_spent; // assuming time is stored in seconds
+};
+
+class Commands : BaseModel
+{
+public:
+    Commands() : BaseModel("commands", {{"command_name", "TEXT"}, {"command_description", "TEXT"}})
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("command_name", &Commands::command_name),
+                                               make_column("command_description", &Commands::command_description)));
+        storage.sync_schema(true);
+    }
+
+    std::string help(std::string &command_name)
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("command_name", &Commands::command_name),
+                                               make_column("command_description", &Commands::command_description)));
+        auto command = storage.get<Commands>(command_name);
+        return command.command_description;
+    }
+
+    void add(std::string &command_name, std::string &command_description)
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("command_name", &Commands::command_name),
+                                               make_column("command_description", &Commands::command_description)));
+        Commands new_command;
+        new_command.command_name = command_name;
+        new_command.command_description = command_description;
+        storage.insert(new_command);
+    }
+
+    void Commands::removeCommand(std::string &command_name)
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("command_name", &Commands::command_name),
+                                               make_column("command_description", &Commands::command_description)));
+        storage.remove<Commands>(&Commands::command_name, command_name);
+    }
+
+    void update(std::string &new_command_name, std::string &old_command_name)
+    {
+        auto storage = make_storage("concept.db",
+                                    make_table(table_name,
+                                               make_column("command_name", &Commands::command_name),
+                                               make_column("command_description", &Commands::command_description)));
+        auto command = storage.get<Commands>(old_command_name);
+        command.command_name = new_command_name;
+        storage.update(command);
+    }
+
+private:
+    std::string command_name;
+    std::string command_description;
 };
